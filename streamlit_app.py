17e39d2f8aa4560d6d45cebea3b573f91a72fdea
polxyxnn <134563676+polxyxnn@users.noreply.github.com>
2026-03-05 06:47:07 +0000

    sample3

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,20 +19,20 @@ logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 dashboard = st.Page(
     "reports/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True
 )
-bugs = st.Page("reports/bugs.py", title="Bug reports", icon=":material/bug_report:")
-alerts = st.Page(
-    "reports/alerts.py", title="System alerts", icon=":material/notification_important:"
-)
+# bugs = st.Page("reports/bugs.py", title="Bug reports", icon=":material/bug_report:")
+# alerts = st.Page(
+#     "reports/alerts.py", title="System alerts", icon=":material/notification_important:"
+# )
 
-search = st.Page("tools/search.py", title="Search", icon=":material/search:")
-history = st.Page("tools/history.py", title="History", icon=":material/history:")
+ssa_rocket = st.Page("SSA-tools/rocket.py", title="Rocket Launch", icon=":material/rocket_launch:")
+ssa_collision = st.Page("SSA-tools/collision.py", title="Collision", icon=":material/explosion:")
 
 if st.session_state.logged_in:
     pg = st.navigation(
         {
             "Account": [logout_page],
-            "Reports": [dashboard, bugs, alerts],
-            "Tools": [search, history],
+            "Reports": [dashboard],
+            "Tools": [ssa_rocket, ssa_collision],
         }
     )
 else:
