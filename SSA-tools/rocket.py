14be9d9b4bf6c6c2b7f6a65e24353d2126e1614b
polxyxnn <134563676+polxyxnn@users.noreply.github.com>
2026-03-06 03:43:51 +0000

    sample24

--- SSA-tools/rocket.py
+++ SSA-tools/rocket.py
@@ -12,109 +12,109 @@ from shapely.geometry import Polygon
 from datetime import datetime, timedelta, date
 from streamlit_folium import st_folium
 
-StreamlitPage.run()
+st.set_page_config(page_title="Rocket Launch Monitoring and Dropzone Mapping", page_icon="🚀")
 
-# -----------------------------
-# LOGO
-# -----------------------------
+# # -----------------------------
+# # LOGO
+# # -----------------------------
 
-logo_url = "utils/PhilSA_v5_White.png"
+# logo_url = "utils/PhilSA_v5_White.png"
 
-st.logo(
-    image=logo_url,
-    size="large"
-)
+# st.logo(
+#     image=logo_url,
+#     size="large"
+# )
 
-# -----------------------------
-# SIDEBAR STYLE
-# -----------------------------
-st.markdown(
-    """
-    <style>
-        section[data-testid="stSidebar"] {
-            width: 300px !important;
-        }
+# # -----------------------------
+# # SIDEBAR STYLE
+# # -----------------------------
+# st.markdown(
+#     """
+#     <style>
+#         section[data-testid="stSidebar"] {
+#             width: 300px !important;
+#         }
 
-        [data-testid="stSidebarNav"]::before {
-            content: "Space Situational Awareness";
-            font-size: 22px;
-            font-weight: 600;
-            display: block;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+#         [data-testid="stSidebarNav"]::before {
+#             content: "Space Situational Awareness";
+#             font-size: 22px;
+#             font-weight: 600;
+#             display: block;
+#             text-align: center;
+#             margin-bottom: 20px;
+#         }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
-# -----------------------------
-# SESSION STATE
-# -----------------------------
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-
-# -----------------------------
-# LOGIN / LOGOUT
-# -----------------------------
-def login():
-    st.title("Login")
-
-    if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+# # -----------------------------
+# # SESSION STATE
+# # -----------------------------
+# if "logged_in" not in st.session_state:
+#     st.session_state.logged_in = False
 
 
-def logout():
-    st.title("Logout")
+# # -----------------------------
+# # LOGIN / LOGOUT
+# # -----------------------------
+# def login():
+#     st.title("Login")
 
-    if st.button("Log out"):
-        st.session_state.logged_in = False
-        st.rerun()
-
-
-# -----------------------------
-# PAGES
-# -----------------------------
-login_page = st.Page(login, title="Log in", icon=":material/login:")
-logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
-
-dashboard = st.Page(
-    "reports/dashboard.py",
-    title="Dashboard",
-    icon=":material/dashboard:",
-    default=True
-)
-
-ssa_rocket = st.Page(
-    "SSA-tools/rocket.py",
-    title="Rocket Launch",
-    icon=":material/rocket_launch:"
-)
-
-ssa_collision = st.Page(
-    "SSA-tools/collision.py",
-    title="Collision",
-    icon=":material/compare_arrows:"
-)
+#     if st.button("Log in"):
+#         st.session_state.logged_in = True
+#         st.rerun()
 
 
-# -----------------------------
-# NAVIGATION
-# -----------------------------
-if st.session_state.logged_in:
-    pg = st.navigation(
-        {
-            "Account": [logout_page],
-            "Reports": [dashboard],
-            "Tools": [ssa_rocket, ssa_collision],
-        }
-    )
-else:
-    pg = st.navigation([login_page])
+# def logout():
+#     st.title("Logout")
 
-pg.run()
+#     if st.button("Log out"):
+#         st.session_state.logged_in = False
+#         st.rerun()
+
+
+# # -----------------------------
+# # PAGES
+# # -----------------------------
+# login_page = st.Page(login, title="Log in", icon=":material/login:")
+# logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+
+# dashboard = st.Page(
+#     "reports/dashboard.py",
+#     title="Dashboard",
+#     icon=":material/dashboard:",
+#     default=True
+# )
+
+# ssa_rocket = st.Page(
+#     "SSA-tools/rocket.py",
+#     title="Rocket Launch",
+#     icon=":material/rocket_launch:"
+# )
+
+# ssa_collision = st.Page(
+#     "SSA-tools/collision.py",
+#     title="Collision",
+#     icon=":material/compare_arrows:"
+# )
+
+
+# # -----------------------------
+# # NAVIGATION
+# # -----------------------------
+# if st.session_state.logged_in:
+#     pg = st.navigation(
+#         {
+#             "Account": [logout_page],
+#             "Reports": [dashboard],
+#             "Tools": [ssa_rocket, ssa_collision],
+#         }
+#     )
+# else:
+#     pg = st.navigation([login_page])
+
+# pg.run()
 
 # ====================== PDF SUPPORT ======================
 try:
@@ -444,7 +444,7 @@ def create_folium_map(launch_site_value, dropzones, shape_dir):
     return m
 
 # ====================== MAIN APP ======================
-st.title("Rocket Launch Monitoring and Dropzone Mapping")
+# st.title("Rocket Launch Monitoring and Dropzone Mapping")
 
 # ====================== LIVE MAP PREVIEW (AT THE TOP) ======================
 st.subheader("📍 Live Rocket Launch Dropzone Map")
